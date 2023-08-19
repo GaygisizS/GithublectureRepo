5cf06bb495ea642f5ab23ca9ed48ef51d54ac7c6
Gaygisiz S <gaygisizs82@gmail.com>
2023-08-19 21:21:23 +0300

    V.01.02

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -2,5 +2,9 @@ public class Class01 {
     public static void main(String[] args) {
         //Versiyon 1 kodlar
         System.out.println("Versiyon 1");
+
+        // Versiyon 2 kodlaru
+        System.out.println("Versiyon 2");
+
     }
 }
